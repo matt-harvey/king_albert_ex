b079e5d6396fe699dc226ac28c2c441eb0a5e3e6
Matthew Harvey <software@matthewharvey.net>
2020-08-30 08:13:52 +1000

    Finish tests for KingAlbertEx.Column module

--- test/king_albert_ex/column_test.exs
+++ test/king_albert_ex/column_test.exs
@@ -65,6 +65,21 @@ defmodule KingAlbertEx.ColumnTest do
   test "displayed_cards", %{column: column} do
     assert Column.displayed_cards(column) == [" 2♣", " J♣", " Q♡", " K♠", "10♢", " A♡", " 3♣"]
   end
-end
 
-# TODO Test the @behaviour implementation.
+  test "_can_give?", %{column: column} do
+    assert Column._can_give?(column) == true
+    assert Column._can_give?(%Position{kind: Column, cards: []}) == false
+  end
+
+  test "_can_receive?", %{column: column} do
+    assert Column._can_receive?(column, {2, :hearts}) == true
+    assert Column._can_receive?(column, {2, :clubs}) == false
+    assert Column._can_receive?(column, {2, :diamonds}) == true
+    assert Column._can_receive?(column, {2, :spades}) == false
+    assert Column._can_receive?(column, {3, :hearts}) == false
+    assert Column._can_receive?(column, {4, :hearts}) == false
+    assert Column._can_receive?(column, {1, :clubs}) == false
+    assert Column._can_receive?(column, {1, :hearts}) == false
+    assert Column._can_receive?(%Position{kind: Column, cards: []}, {9, :clubs}) == true
+  end
+end
